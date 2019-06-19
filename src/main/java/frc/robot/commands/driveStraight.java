7b9f1e863786fd97a89fc5a3377c988d162c0d38
rettaM0intA <52013739+rettaM0intA@users.noreply.github.com>
2019-06-19 18:30:00 -0400

    Move bot forward
    do the thing

---
+++ src/main/java/frc/robot/commands/driveStraight.java
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+
+public class driveStraight extends Command {
+  public double m_leftSpeed,m_rightSpeed,m_movementTime;
+  public double timeResetValue;
+  public driveStraight(){
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.m_driveSubsystem);
+    // m_leftSpeed = leftSpeed;
+    // m_rightSpeed = rightSpeed;
+    // m_movementTime = movementTime;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    timeResetValue = Timer.getFPGATimestamp();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+
+    
+    // Robot.m_driveSubsystem.tankDrive(0.50,0.50);
+    SmartDashboard.putNumber("time",Timer.getFPGATimestamp());
+
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    // if(m_movementTime <= Timer.getFPGATimestamp() - timeResetValue){
+    //   return true;
+    // }
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
--- src/main/java/frc/robot/subsystems/driveSubsystem.java
+++ src/main/java/frc/robot/subsystems/driveSubsystem.java
@@ -7,18 +7,40 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.commands.driveStraight;
 
 /**
  * Add your docs here.
  */
 public class driveSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+  // here. Call these from Commands.  
+  public WPI_TalonSRX frontLeft = new WPI_TalonSRX(46);
+	public WPI_TalonSRX	frontRight = new WPI_TalonSRX(47);
+	public WPI_TalonSRX	backLeft = new WPI_TalonSRX(44);
+  public WPI_TalonSRX	backRight = new WPI_TalonSRX(49);
+
+  public ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
+  public SpeedControllerGroup rightSpeedControllerGroup = new SpeedControllerGroup(frontRight,backRight);
+  public SpeedControllerGroup LeftSpeedControllergroup = new SpeedControllerGroup(frontLeft,backLeft);
+
+  public DifferentialDrive drive = new DifferentialDrive(LeftSpeedControllergroup, rightSpeedControllerGroup);
+
+  public void tankDrive(double leftSpeed,double rightSpeed){
+    drive.tankDrive(leftSpeed,rightSpeed);
+  }
 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+   setDefaultCommand(new driveStraight());
+
   }
 }
